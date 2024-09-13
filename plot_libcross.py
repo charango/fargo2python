a11dfeaa5716e00203fefaa2dd6906dd4ec51b1a
charango <44603170+charango@users.noreply.github.com>
2024-09-13 18:27:57 +0200

    Add files via upload

--- plot_libcross.py
+++ plot_libcross.py
@@ -152,7 +152,7 @@ def plotlibcross():
             xs = 1.1* rpla * np.sqrt(mp/hp)          # half-width of planet's horseshoe region
             nup = alphaviscosity*hp*hp*np.sqrt(rpla) # turbulent kinematic viscosity at planet
             #tau_visc = 0.1*xs*xs/nup                 # viscous timescale across planet's HS region (scaled down here)
-            tau_visc = xs*xs/nup                    # viscous timescale across planet's HS region (in code units)
+            tau_visc = 0.5*xs*xs/nup                    # viscous timescale across planet's HS region (in code units)
             '''
             if k < len(on)-1:
                 rpla_p1 = np.sqrt( xpla[take_one_point_every*k+1]*xpla[take_one_point_every*k+1] + ypla[take_one_point_every*k+1]*ypla[take_one_point_every*k+1] )
