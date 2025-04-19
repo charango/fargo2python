e333717e5701da209acbda6f2d528d1e7ee35556
charango <44603170+charango@users.noreply.github.com>
2025-04-19 14:44:58 +0200

    improved torque plot
    plot_tqwk can now be 'torque', 'power' or 'indtorque' (disc's indirect torque on planet). For each case, a running-time average can be displayed, and the final torque quantity can be optionally divided by the reference torque value 'Gamma_0' (Eq. 2 in Baruteau+ 2014 PPVI review).

--- plot_tqwk.py
+++ plot_tqwk.py
@@ -18,17 +18,25 @@ def plottqwk():
     plt.subplots_adjust(left=0.17, right=0.94, top=0.94, bottom=0.12)
     ax = fig.gca()
     ax.set_xlabel('time [orbits]')
+
     if par.plot_tqwk == 'torque':
         ytitle = 'Specific torque on planet'
-    if par.plot_tqwk == 'normtorque':
-        ytitle = r'$\Gamma / \Gamma_0$'
-    if par.plot_tqwk == 'rtatorque':
-        ytitle = 'Specific r.t.a. torque on planet'
+        if par.normalize_torque == 'Yes':
+            ytitle = r'$\Gamma / \Gamma_0$'
+
+    if par.plot_tqwk == 'indtorque':
+        ytitle = 'Specific indirect torque on planet'
+        if par.normalize_torque == 'Yes':
+            ytitle = r'$\Gamma_{\rm ind} / \Gamma_0$'
+
     if par.plot_tqwk == 'power':
         ytitle = 'Specific power on planet'
-    if par.plot_tqwk == 'rtapower':
-        ytitle = 'Specific r.t.a. power on planet'
+        
+    if par.running_time_average == 'Yes':
+        ytitle = 'r.t.a. '+ytitle
+
     ax.set_ylabel(ytitle)
+
     ax.tick_params(top='on', right='on', length = 5, width=1.0, direction='out')
     plt.ticklabel_format(axis='y', style='scientific', scilimits=(0,0))
     
@@ -67,7 +75,7 @@ def plottqwk():
         print('nbplanets = ',nbplanets)
 
         # Normalized torque by Gamma_0 = q/h^2 x Sigma(r_p) r_p^4 Omega^2(r_p)
-        if par.plot_tqwk == 'normtorque':
+        if par.normalize_torque == 'Yes':
             # get planet-to-star mass ratio q
             q = mpla[len(mpla)-1]   # time-varying array
             # get local disc's aspect ratio
@@ -100,16 +108,27 @@ def plottqwk():
         # now, read tqwk0.dat file
         for k in range(nbplanets): 
             f1, it, ot, f4, f5, ip, op, f8, f9, time = np.loadtxt(directory[j]+"/tqwk"+str(k)+".dat",unpack=True)
-            time /= (2.0*np.pi*rpla_0*np.sqrt(rpla_0))  # time in orbital periods at inner planet's initial location
             tq = it+ot
             pw = ip+op
-            if par.plot_tqwk == 'torque' or par.plot_tqwk == 'rtatorque' or par.plot_tqwk == 'normtorque':
+            
+            if par.plot_tqwk == 'torque':
                 y = tq
-            if par.plot_tqwk == 'power' or par.plot_tqwk == 'rtapower':
+            if par.plot_tqwk == 'power':
                 y = pw
-            if par.plot_tqwk == 'rtatorque' or par.plot_tqwk == 'rtapower':
+            # April 2025: disc indirect torque on planet
+            if par.plot_tqwk == 'indtorque':
+                f1, y, time = np.loadtxt(directory[j]+"/indtq"+str(k)+".dat",unpack=True)
+            
+            if par.normalize_torque == 'Yes':
+                y /= Gamma_0
+            if par.running_time_average == 'Yes':
+                buf = y
                 for i in range(1,len(y)):
-                    y[i] = (i*y[i-1] + tq[i])/(i+1.0)
+                    y[i] = (i*y[i-1] + buf[i])/(i+1.0)
+            
+            # time in orbital periods at inner planet's initial location
+            time /= (2.0*np.pi*rpla_0*np.sqrt(rpla_0))  
+
             if (par.mytmin == '#'):
                 xmin = time.min()
                 imin = 0.0
