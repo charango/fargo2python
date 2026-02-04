5eab2572a68c824299043a2cf9e240c7463b4138
charango <44603170+charango@users.noreply.github.com>
2026-02-04 10:04:32 +0100

    Add files via upload

--- plot_disccom.py
+++ plot_disccom.py
@@ -192,7 +192,7 @@ def plotdisccom():
                 if par.central_binary == 'Yes':
                     x_com[k] = np.sum(mass*X) / np.sum(mass)
                     y_com[k] = np.sum(mass*Y) / np.sum(mass)
-                    pa_com[k] = math.atan2(y_com[k],x_com[k])
+                    pa_com[k] = math.atan2(y_com[k],x_com[k])+np.pi
                     t_com[k] = round(date[k*take_one_point_every]/2./np.pi,1)
 
                 #print(mp*xp, np.sum(mass*X), mp*xp+np.sum(mass*X))
@@ -329,7 +329,7 @@ def plotdisccom():
             ax.scatter(t_com[1:len(t_com)-1], r_com[1:len(t_com)-1], s=20, marker='+', alpha=1.0, color=par.c20[j],label=mylabel)
         if par.central_binary == 'Yes' and par.plot_disccom == 'tpa':
             ax.scatter(t_com, pa_com, s=30, marker='+', alpha=1.0, color=par.c20[j],label=mylabel)
-            ax.set_xlim(pa_com.min(),pa_com.max())
+            ax.set_ylim(pa_com.min(),pa_com.max())
 
         # save data in ascii file
         fileout = open('log10rcom_'+str(directory[j])+'.dat','w')
